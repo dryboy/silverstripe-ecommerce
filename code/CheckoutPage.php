f3176bea8d8d0486f080933c128067c6a1cbd210
jedateach <jeremy@burnbright.net>
2010-11-07 21:21:46 +0000

    variations fixes, spelling fixes
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/branches/burnbright_variations@296 950b781c-c544-efab-2f99-90083ee69683

--- code/CheckoutPage.php
+++ code/CheckoutPage.php
@@ -96,7 +96,7 @@ class CheckoutPage extends Page {
 			new HtmlEditorField('AlreadyCompletedMessage', 'Already Completed - shown when the customer tries to checkout an already completed order', $row = 4),
 			new HtmlEditorField('NonExistingOrderMessage', 'Non-existing Order - shown when the customer tries ', $row = 4),
 			new HtmlEditorField('MustLoginToCheckoutMessage', 'MustLoginToCheckoutMessage', $row = 4),
-			new HtmlEditorField('PurchaseComplete', 'Purchase Complete - included in reciept email, after the customer submits the checkout ', $row = 4),
+			new HtmlEditorField('PurchaseComplete', 'Purchase Complete - included in receipt email, after the customer submits the checkout ', $row = 4),
 			new HtmlEditorField('ChequeMessage', 'Cheque Message - shown when a customer selects a delayed payment option (such as a cheque payment) ', $rows = 4)
 		));
 
